3d3263dceb461556fa3d1d05f31c227b56d1e655
kiran.sarella <kiran.sarella@gmail.com>
2014-04-21 15:24:36 +0530

    Initial commit

---
+++ GitTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitTest
+//
+//  Created by Maitreya Tech 750 on 21/04/14.
+//  Copyright (c) 2014 MI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitTest
+//
+//  Created by Maitreya Tech 750 on 21/04/14.
+//  Copyright (c) 2014 MI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
